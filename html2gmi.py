576fa8cdff66665e76f3fdcec58661d35dfbe993
Silas S. Brown <ssb22@cam.ac.uk>
2026-04-06 10:56:05 +0100

    support multiple dd elements after dt

--- html2gmi.py
+++ html2gmi.py
@@ -2,7 +2,7 @@
 # (should work on both Python 2 and Python 3)
 
 """Convert simple HTML pages into Gemini or Markdown, with typography
-Version 1.64 (c) 2021-26 Silas S. Brown.  License: Apache 2"""
+Version 1.65 (c) 2021-26 Silas S. Brown.  License: Apache 2"""
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -81,11 +81,11 @@ for n in range(1,7):
         if len(sharps) < 3: sharps += "#"
 d = re.sub("</[hH][^>]*>","\n",d)
 n_stack = []
-def indents(cut): return ''.join("%@quotS"*(len(str(i))+(2 if i else 1)) for i in n_stack[:cut])
+def indents(cut): return ''.join("%@quotS"*(len(str(i if i else 0))+(2 if i else 1)) for i in n_stack[:cut])
 def number(m):
     m = m.group()
     if "<li" in m.lower() or "<dt" in m.lower():
-        if "<dt" in m.lower(): n_stack.append(0)
+        if "<dt" in m.lower(): n_stack.append(False)
         if n_stack[-1]: n_stack[-1] += 1 # whether markdown_mode or not
         if markdown_mode: r=indents(-1)+(str(n_stack[-1]-1)+'.' if n_stack[-1] else "*")
         elif n_stack[-1]: r = ''.join(str(i-1)+'.' for i in n_stack if i) # i-1 for higher-up options too because we've incremented the counter and we want a sub-point of the old counter
@@ -94,10 +94,10 @@ def number(m):
     elif "</li" in m.lower(): return ""
     elif "<ul" in m.lower(): n_stack.append(0)
     elif "<ol" in m.lower(): n_stack.append(1)
-    elif "</ul" in m.lower() or "</ol" in m.lower() or "</dd" in m.lower(): n_stack.pop()
+    elif "</ul" in m.lower() or "</ol" in m.lower() or ("</dd" in m.lower() and n_stack and n_stack[-1]==False): n_stack.pop() # </dd> more complex because can have two <dd> elements after a <dt> and we're not seeing the opening
     elif markdown_mode: return "\n"+indents(None)
     return "\n"
-d = re.sub(r"(?i)\s*</dt>\s*","",re.sub("(?i)<dd([^a-z>][^>]*)?>",":<dd> ",d)).replace("::<dd>",":").replace(":<dd>",":") # dt-dd transition (but don't add second : if already one there) - important to do this before number, which takes <dt> and </dd>
+d = re.sub(r"</dd>\s*:\s*<dd>","<br>",re.sub(r"(?i)\s*</dt>\s*","",re.sub("(?i)<dd([^a-z>][^>]*)?>",":<dd> ",d))).replace("::<dd>",":").replace(":<dd>",":") # dt-dd transition (but don't add second : if already one there) - important to do this before number, which takes <dt> and </dd>
 d = re.sub("(?i)</?([uo]l|li|p|div|details|summary|br|d[td])([^>a-z][^>]*)?>",number,d)
 d = re.sub("(?i)<blockquote([^a-z>][^>]*)?>","\n> ",d)
 d = re.sub("(?i)</?pre([^a-z>][^>]*)?>","\n"+protect("```")+"\n",d)
